3a02a42686bbeba0754e7be2f04845a13f9f8c29
Adamnmn <Adamc119@nycstudents.net>
2023-09-26 12:27:31 -0400

    hey

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,24 @@
-public class HelloWorld {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+public class HelloWorld {
+    public static void main(String[] args)
+
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Total Bill:");
+        int total = scan.nextInt();
+        System.out.println("Tip Percentage:");
+        double tipPercent = scan.nextDouble();
+        System.out.println("Number of People:");
+        int peopleNum = scan.nextInt();
+
+        double tip = tipPercent / 100;
+        double tipAmount = total * tip;
+        double finalTotal = (tipAmount * peopleNum) + total;
+
+        System.out.println("Final Total:" + finalTotal);
+
     }
-}
+
+
+    }
