f7a6d62e8659fa3ca8d4bea0c0dc73032259973c
Adamnmn <Adamc119@nycstudents.net>
2023-09-27 11:28:00 -0400

    hey

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,20 +5,23 @@ public class HelloWorld {
 
     {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Total Bill:");
+        System.out.println("What Is The Total Bill:");
         int total = scan.nextInt();
-        System.out.println("Tip Percentage:");
+        System.out.println("What Is The Tip Percentage:");
         double tipPercent = scan.nextDouble();
-        System.out.println("Number of People:");
+        System.out.println("What Is The Number of People:");
         int peopleNum = scan.nextInt();
 
         double tip = tipPercent / 100;
         double tipAmount = total * tip;
         double finalTotal = (tipAmount * peopleNum) + total;
+        double totalPerPerson =  (tipAmount+total)/peopleNum;
+        double totalTip = (tipAmount*peopleNum);
 
-        System.out.println("Total Tip:" + (tipAmount*peopleNum));
+
+        System.out.println("Total Tip:" + totalTip );
         System.out.println("Tip Per Person:" + tipAmount);
-        System.out.println("Total Per Person:" + (tipAmount+total)/peopleNum);
+        System.out.println("Total Per Person:" + totalPerPerson);
         System.out.println("Final Total:" + finalTotal);
 
 
